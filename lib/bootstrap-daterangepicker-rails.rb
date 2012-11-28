0b3647fea690d1d440e4ed9eb39da27b6b267ee6
Jordan Brock <jordan@brock.id.au>
2012-11-28 14:51:32 +0800

    Initial import of the bootstrap-daterangepicker files from github.com/dangrossman/daterangepicker

---
+++ lib/bootstrap-daterangepicker-rails.rb
@@ -0,0 +1,11 @@
+require 'bootstrap-daterangepicker-rails/version'
+
+module Bootstrap
+  module Daterangepicker
+    module Rails
+      class Engine < ::Rails::Engine
+
+      end
+    end
+  end
+end
---
+++ lib/bootstrap-daterangepicker-rails/version.rb
@@ -0,0 +1,7 @@
+module Bootstrap
+  module Daterangepicker
+    module Rails
+      VERSION = '0.0.3'
+    end
+  end
+end
